1f91f579eadf2b78cc426b3e0e75d0f1fcbac833
jh-117 <limjiahuey2004@gmail.com>
2026-01-23 00:36:39 +0800

    Updated LandingPage.tsx

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -86,13 +86,7 @@ export const LandingPage: React.FC<LandingPageProps> = ({ onStart, onBrowseTempl
               <Sparkles className="w-4 h-4 text-cyan-500 animate-pulse" />
             </div>
             <div className="flex space-x-4">
-              <button
-                onClick={onBrowseTemplates}
-                className="px-4 py-2 text-gray-700 hover:text-blue-600 font-medium transition-colors relative group"
-              >
-                Browse Templates
-                <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-blue-600 group-hover:w-full transition-all duration-300"></span>
-              </button>
+              
             </div>
           </div>
         </div>
